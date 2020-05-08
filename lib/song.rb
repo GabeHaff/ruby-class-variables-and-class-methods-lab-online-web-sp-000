d6bc3cfd2cfe23e334c301c201f6df00328fee98
Gabriel Hafferty <gabe.hafferty.business@gmail.com>
2020-05-08 11:00:06 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -9,4 +9,33 @@ class Song
   def self.count
     @@count
   end 
- 
+ def self.artists
+    @@artists & @@artists
+  end
+
+  def self.genres
+    @@genres & @@genres
+  end
+
+  def self.genre_count
+    @@genre_count
+  end
+
+  def self.artist_count
+    @@artist_count
+  end
+
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@count += 1
+    @@genres << genre
+    @@artists << artist
+    @@genre_count[genre] ||= 0
+    @@genre_count[genre] += 1
+    @@artist_count[artist] ||= 0
+    @@artist_count[artist] += 1
+  end
+
+end
